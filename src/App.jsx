d38dcba66f1586e45dc85276270d48f94203f661
salkhaleeli <saad@Saads-MacBook-Pro.local>
2023-06-26 14:24:41 -0600

    adding TodoList

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import "./styles.css"
 import "./NewTodoForm"
 import { NewTodoForm } from "./NewTodoForm";
+import { TodoList } from "./TodoList";
 
 export default function App() {
   const [todos, setTodos] = useState([]);
@@ -31,29 +32,11 @@ export default function App() {
       return currentTodos.filter(todo => todo.id !== id)
     })
   }
-
   return (
     <>
       <NewTodoForm onSubmit={addTodo} />
-
       <h1>Todo List</h1>
-      <ul className="list">
-        {todos.length === 0 && 'No Todos'}
-        {todos.map(todo => {
-          return (
-            <li key={todo.id}>
-              <label>
-                <input type='checkbox' checked={todo.completed}
-                  onChange={e => toggleTodo(todo.id, e.target.checked)}
-                />
-                {todo.title}
-              </label>
-              <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
-            </li>
-          )
-
-        })}
-      </ul>
+      <TodoList todos={todos} />
     </>
   )
 }
